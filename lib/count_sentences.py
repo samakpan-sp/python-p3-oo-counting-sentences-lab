acdad00523e5d1b6c93f6eaef9a4fa7a1dbf27be
mrSam <samakpan817@gmail.com>
2024-02-06 12:01:26 +0100

    Done with this task.

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,30 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = str(value)
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = re.split(r'[.!?]', self.value)
+        xs = [sentence.strip() for sentence in sentences if sentence.strip()]
+        return len(xs)
+
+# Example usage:
+xs = MyString(
+    """You are doing a great job. Keep it up Sam.
+      You will soon be happy with the result of your efforts.
+      Money will never be your problem. You'll be invited for tech talks a lot.
+      The youth, particularly will gain much from you in your entire life.
+      """
+)
+
+print(xs.count_sentences())  # Output: 4
